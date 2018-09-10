0fb2f2d2616b527690509d686efd070e4a61d8e2
zhang <thexwant@gmail.com>
2018-09-10 11:09:05 +0800

    项目初始化

---
+++ src/pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    暂无数据哦
+    <vant-button>按钮</vant-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+
+  components: {
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  span {
+    color: blue;
+  }
+</style>
---
+++ src/pages/promise-list/main.js
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+import PromiseList from './promise-list'
+const app = new Vue(PromiseList);
+
+app.$mount();
